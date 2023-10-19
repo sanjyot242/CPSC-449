8976923b72f1a9903621a2498af02aaf1db0bbc6
sanjyot242 <sanjyot.satvi242@gmail.com>
2023-10-18 18:14:31 -0700

    update till Exercise 2

--- Exercise2/fastapi/api/bin/init.sh
+++ Exercise2/fastapi/api/bin/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sqlite3 ./var/books.db < ./share/books.sql
+sqlite3 ./var/primary/fuse/books.db < ./share/books.sql
--- Exercise2/fastapi/api/bin/post.sh
+++ Exercise2/fastapi/api/bin/post.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-http --verbose POST localhost:5000/books/ @"$1"
+http --verbose POST localhost:5200/api/books/ @"$1"
